7ff6ea31445a8d76630179d67b3433dfb3a572b9
Andexter8 <8crafteryt@gmail.com>
2024-12-24 18:54:01 -0800

    Made a sitemap.xml file.

---
+++ sitemap_generator.js
@@ -0,0 +1,24 @@
+const SitemapGenerator = require('sitemap-generator');
+
+// create generator
+const generator = SitemapGenerator('https://www.dev.8crafter.com/', {
+  stripQuerystring: false
+});
+
+const crawler = generator.getCrawler()
+const sitemap = generator.getSitemap()
+
+sitemap.addURL('https://www.dev.8crafter.com/debug-sticks-add-on/andexdbnbtstructureloader')
+
+generator.on('add', (url) => {
+  console.log(url);
+  // log url
+});
+
+// register event listeners
+generator.on('done', () => {
+  // sitemaps created
+});
+
+// start the crawler
+generator.start();
