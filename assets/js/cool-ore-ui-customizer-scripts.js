0fe25ca684f21e590ccbbcd18117706730c1a87a
8Crafter (PC) <8crafteryt@gmail.com>
2025-04-15 22:10:54 -0700

    Added cool-ore-ui-customizer-scripts.js.

---
+++ assets/js/cool-ore-ui-customizer-scripts.js
@@ -0,0 +1,142 @@
+// RGB Ore UI
+(async function cycleHueRotate(
+    stopOnCondition = function () {
+        return false;
+    },
+    interval = 20,
+    step = 1
+) {
+    let val = 0;
+    return new Promise(function resolve() {
+        let id = setInterval(function () {
+            if (stopOnCondition() == true) {
+                clearInterval(id);
+                resolve(true);
+                return;
+            }
+
+            val += step;
+            val %= 360;
+
+            document.body.style.filter = `hue-rotate(${val}deg)`;
+        }, interval);
+    });
+})();
+
+// Spinning Ore UI
+(async function spinBody(
+    stopOnCondition = function () {
+        return false;
+    },
+    interval = 20,
+    step = 1
+) {
+    let val = 0;
+    return new Promise(function resolve() {
+        let id = setInterval(function () {
+            if (stopOnCondition() == true) {
+                clearInterval(id);
+                resolve(true);
+                return;
+            }
+
+            val += step;
+            val %= 360;
+
+            document.body.style.transform = `rotateZ(${val}deg)`;
+        }, interval);
+    });
+})();
+
+// Cursed Fast Spinning Ore UI
+(async function spinBodyFastCursed(
+    stopOnCondition = function () {
+        return false;
+    },
+    interval = 1,
+    step = 1
+) {
+    let val = 0;
+    return new Promise(function resolve() {
+        let id = setInterval(function () {
+            if (stopOnCondition() == true) {
+                clearInterval(id);
+                resolve(true);
+                return;
+            }
+
+            val += step;
+            val %= 360;
+
+            document.getElementsByTagName("html")[0].style.transform = `rotateZ(${val}deg)`;
+        }, interval);
+    });
+})();
+
+// Broken Ore UI
+(async function spinBodyFastCursed(
+    stopOnCondition = function () {
+        return false;
+    },
+    interval = 1
+) {
+    let val = 0;
+    return new Promise(function resolve() {
+        let id = setInterval(function () {
+            if (stopOnCondition() == true) {
+                clearInterval(id);
+                resolve(true);
+                return;
+            }
+
+            val += Math.floor(Math.random() * 360);
+
+            document.body.style.transform = `rotateZ(${val}deg)`;
+        }, interval);
+    });
+})();
+
+// Broken Ore UI (Stays on screen after rotating)
+(async function spinBodyFastCursed(
+    stopOnCondition = function () {
+        return false;
+    },
+    interval = 50
+) {
+    let val = 0;
+    return new Promise(function resolve() {
+        let id = setInterval(function () {
+            if (stopOnCondition() == true) {
+                clearInterval(id);
+                resolve(true);
+                return;
+            }
+
+            val += Math.floor(Math.random() * 360);
+
+            document.getElementsByTagName("html")[0].style.transform = `rotateZ(${val}deg)`;
+        }, interval);
+    });
+})();
+
+// Make everything have a random font size.
+document.body.querySelectorAll("*").forEach((/** @type {HTMLElement} */ element) => {
+    element.style.fontSize = (Math.floor(Math.random() * 30) + 10) + "px";
+});
+
+// Make everything have a random font size every second.
+setInterval(() => {
+    document.body.querySelectorAll("*").forEach((/** @type {HTMLElement} */ element) => {
+        element.style.fontSize = (Math.floor(Math.random() * 30) + 10) + "px";
+    });
+}, 100);
+
+// Make everything have a random font size every second, but the variation increases over time.
+let fontSizeVariation = 30;
+setInterval(() => {
+    fontSizeVariation += 1;
+    fontSizeVariation %= 300;
+    document.body.querySelectorAll("*").forEach((/** @type {HTMLElement} */ element) => {
+        element.style.fontSize = (Math.floor(Math.random() * fontSizeVariation) + 10) + "px";
+    });
+}, 100);
